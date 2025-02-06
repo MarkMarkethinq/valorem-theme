a2cee50c1007ab17b16034e0983b20e0808d29b1
MarkMarkethinq <mark@markethinq.nl>
2025-02-06 16:47:00 +0100

    updates style

--- resources/blocks/diensten.php
+++ resources/blocks/diensten.php
@@ -1,23 +1,23 @@
-<section id="diensten" class="bg-white dark:bg-gray-900">
+<section id="diensten" class="bg-white">
   <div class="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 xl:gap-16 sm:py-16 lg:px-6 ">
     <?php if (get_field('grote_afbeelding')): ?>
       <img class="mb-4 w-full lg:mb-0 rounded-lg" src="<?php echo get_field('grote_afbeelding')['url'] ?>" alt="<?php echo get_field('grote_afbeelding')['alt'] ?>">
     <?php endif; ?>
-      <div class="text-gray-500 dark:text-gray-400 sm:text-lg">
-          <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white"><?php echo get_field('titel') ?></h2>
+      <div class="text-gray-500 sm:text-lg">
+          <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900"><?php echo get_field('titel') ?></h2>
           <p class="mb-8 font-light lg:text-xl"><?php echo get_field('tekst') ?></p>
-          <div class="py-8 mb-6 border-t border-b border-gray-200 dark:border-gray-700">
+          <div class="py-8 mb-6 border-t border-b border-gray-200">
               <?php if(have_rows('diensten')): ?>
                 <?php while(have_rows('diensten')): the_row(); ?>
                   <div class="flex pt-8">
-                      <div class="flex justify-center items-center mr-4 w-8 h-8 rounded-full bg-primary-100 dark:bg-primary-900 shrink-0">
+                      <div class="flex justify-center items-center mr-4 w-8 h-8 rounded-full bg-primary-100 shrink-0">
                           <?php if(get_sub_field('icoon')): ?>
                             <img src="<?php echo get_sub_field('icoon')['url']; ?>" alt="<?php echo get_sub_field('icoon')['alt']; ?>" class="w-5 h-5">
                           <?php endif; ?>
                       </div>
                       <div>
-                          <h3 class="mb-2 text-xl font-bold text-gray-900 dark:text-white"><?php echo get_sub_field('titel'); ?></h3>
-                          <p class="mb-2 font-light text-gray-500 dark:text-gray-400"><?php echo get_sub_field('tekst'); ?></p>
+                          <h3 class="mb-2 text-xl font-bold text-gray-900"><?php echo get_sub_field('titel'); ?></h3>
+                          <p class="mb-2 font-light text-gray-500"><?php echo get_sub_field('tekst'); ?></p>
                       </div>
                   </div>
                 <?php endwhile; ?>
--- resources/blocks/over-ons.php
+++ resources/blocks/over-ons.php
@@ -1,10 +1,10 @@
-<section id="over-ons" class="bg-white dark:bg-gray-900">
+<section id="over-ons" class="bg-white">
     <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
-        <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white"><?php echo get_field('titel') ?></h2>
+        <div class="font-light text-gray-500 sm:text-lg">
+            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900"><?php echo get_field('titel') ?></h2>
             <p class="mb-4"><?php echo get_field('tekst') ?></p>
             <?php if (get_field('knop')): ?>
-            <a href="<?php echo get_field('knop')['url']; ?>" target="<?php echo get_field('knop')['target']; ?>" class="inline-flex items-center py-3 mt-4 px-5 font-medium text-center border border-black text-grey-700 rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-900 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+            <a href="<?php echo get_field('knop')['url']; ?>" target="<?php echo get_field('knop')['target']; ?>" class="inline-flex items-center py-3 mt-4 px-5 font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-900">
                 <?php echo get_field('knop')['title']; ?>
                 <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 </a>
