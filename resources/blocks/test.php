7fc6b059c4dbdd4435ae14ed5e0423b007a117f1
MarkMarkethinq <mark@markethinq.nl>
2025-02-06 12:34:12 +0100

    Voorbeelden acf velden

--- resources/blocks/test.php
+++ resources/blocks/test.php
@@ -1,1 +1,10 @@
 <h1 class="text-4xl font-bold text-center text-red-500"><?php echo get_field('title'); ?></h1>
+
+<?php if (get_field('link')): ?>
+    <a href="<?php echo get_field('link')['url']; ?>" class="btn btn-primary"><?php echo get_field('link')['title']; ?></a>
+<?php endif; ?>
+
+<?php if (get_field('image')): ?>
+    <img src="<?php echo get_field('image')['url']; ?>" alt="<?php echo get_field('image')['alt']; ?>" class="w-full h-auto">
+<?php endif; ?>
+
