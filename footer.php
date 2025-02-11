3506ecf850a11055eb665b74680753ada5199f5b
ItsKrusi30 <kristianhuisman@gmail.com>
2025-02-11 15:53:59 +0100

    update

--- footer.php
+++ footer.php
@@ -6,14 +6,28 @@
 
 </main>
 
-<?php 
-// Include feedback tool for logged-in users
-if (is_user_logged_in()) {
-    //get_template_part('includes/feedback-tool/templates/feedback-form');
-}
-?>
+<footer class="bg-white dark:bg-gray-900">
+    <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+         
+        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <div class="sm:flex sm:items-center sm:justify-between">
+            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+                © <?php echo date('Y'); ?> <a href="<?php echo home_url(); ?>" class="hover:underline">Valorem Consultancy </a>. Alle rechten voorbehouden.
+            </span>
+            <div class="flex mt-4 sm:justify-center sm:mt-0">
+                
+                <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
+                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                        <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
+                    </svg>
+                    <span class="sr-only">LinkedIn</span>
+                </a>
+            </div>
+        </div>
+    </div>
+    <script src="<?php echo get_template_directory_uri(); ?>/node_modules/flowbite/dist/flowbite.min.js"></script>
+</footer>
 
 <?php wp_footer(); ?>
-
 </body>
 </html>
--- header.php
+++ header.php
@@ -13,7 +13,7 @@
     |____/ \___| \_/ \___|_|\___/| .__/|_|_| |_|\__, |
                                   |_|            |___/ 
 		-->
-		
+        
   </head>
   <body <?php body_class(); ?>>
 
@@ -28,8 +28,7 @@
                 <?php endif; ?>
             </a>
             <div class="flex items-center lg:order-2">
-                <a href="#" class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log in</a>
-                <a href="#" class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">Get started</a>
+                <a href="#contact" class="text-white bg-primary-900 hover:bg-primary-950 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">Stuur een bericht</a>
                 <button data-collapse-toggle="mobile-menu-2" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
                     <span class="sr-only">Open main menu</span>
                     <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
@@ -40,7 +39,7 @@
                 <?php wp_nav_menu ([
                         'menu' => 'main',
                         'container' => 'ul',
-                        'menu_class' => 'flex gap-4 lg:gap-10 lg:flex-row flex-col py-2 pr-4 pl-3 lg:p-0',
+                        'menu_class' => 'flex lg:gap-8 gap-4 lg:flex-row flex-col py-2 pr-4 pl-3 lg:p-0',
                         'walker' => new WPDocs_Walker_Nav_Menu ()
                     ]);  ?>
             </div>
