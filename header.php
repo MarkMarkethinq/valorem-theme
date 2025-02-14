e68324267f6abf868b4d5ef1d1836ff93b72d6a2
MarkMarkethinq <mark@markethinq.nl>
2025-02-14 11:34:27 +0100

    Header sticky

--- header.php
+++ header.php
@@ -15,12 +15,12 @@
 		-->
         
   </head>
-  <body <?php body_class(); ?>>
+  <body <?php body_class('pt-[60px] lg:pt-[28px]'); ?>>
 
-  <header>
-    <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
+  <header class="fixed w-full top-0 z-50">
+    <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 shadow-md">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <a href="/" class="flex items-center">
+            <a href="/" class="flex items-center lg:w-1/4">
                 <?php if(has_custom_logo()): ?>
                     <div class="h-12 flex items-center">
                         <?php 
@@ -35,7 +35,7 @@
                     <span class="text-xl font-semibold"><?php bloginfo('name'); ?></span>
                 <?php endif; ?>
             </a>
-            <div class="flex items-center lg:order-2">
+            <div class="flex items-center lg:order-3">
                 <a href="#contact" class="text-white bg-primary-900 hover:bg-primary-950 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none">Stuur een bericht</a>
                 <button data-collapse-toggle="mobile-menu-2" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="mobile-menu-2" aria-expanded="false">
                     <span class="sr-only">Open main menu</span>
@@ -43,7 +43,7 @@
                     <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 </button>
             </div>
-            <div class="hidden justify-between items-center pt-2 w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
+            <div class="hidden lg:flex lg:w-2/4 justify-end items-center pt-2 lg:order-2" id="mobile-menu-2">
                 <?php wp_nav_menu ([
                         'menu' => 'main',
                         'container' => 'ul',
