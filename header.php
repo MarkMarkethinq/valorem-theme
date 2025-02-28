1f63da28375f64eb957a6892fc0a42125b2fd249
MarkMarkethinq <mark@markethinq.nl>
2025-02-28 15:11:48 +0100

    Header fix

--- header.php
+++ header.php
@@ -43,7 +43,7 @@
                     <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 </button>
             </div>
-            <div class="hidden lg:flex lg:w-2/4 justify-end items-center pt-2 lg:order-2" id="mobile-menu-2">
+            <div class="hidden justify-end items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
                 <?php wp_nav_menu ([
                         'menu' => 'main',
                         'container' => 'ul',
